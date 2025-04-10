fba427f2830e408c7850c50144c0c7f27ffcf89d
GuilhermeSantosCoelho <guilhermesantoscoelho2000@gmail.com>
2025-04-09 23:09:58 -0300

    fix: middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,7 +40,8 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    request.nextUrl.pathname !== '/'
+    request.nextUrl.pathname !== '/' &&
+    !request.nextUrl.pathname.startsWith('/cliente')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
